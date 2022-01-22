02cbae38a14b58afddcad0dd7cc35d1e596aabb9
Cagil Seker <cagils@gmail.com>
2022-01-23 02:50:02 +0300

    Unsplash Initital Fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,35 @@
-const App = () => 
-  <div className="App">
-    <h1>Images Gallery 2</h1>
-  </div>
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
+const App = () => {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  console.log(UNSPLASH_KEY);
+  return (
+    <div>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+    </div>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
+
+const Header = (props) => {
+  const { title } = props;
+  return (
+    <Navbar style={navbarStyle}>
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
