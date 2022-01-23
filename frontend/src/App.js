0cbe0825ec0e97296de8f5e2ff0a63446a9114b2
Cagil Seker <cagils@gmail.com>
2022-01-23 16:49:42 +0300

    Images displayed in a row

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,7 +19,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -29,6 +32,15 @@ const App = () => {
     <div>
       <Header title='Images Gallery' />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className='mt-4'>
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className='pb-3'>
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
