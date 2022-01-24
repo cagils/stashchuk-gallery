65eb200367f1f33f048e715f7a6b2338b637a73a
Cagil Seker <cagils@gmail.com>
2022-01-24 14:38:33 +0300

    Python formatted

--- api/main.py
+++ api/main.py
@@ -5,37 +5,34 @@ from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='./.env.local')
+load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-UNSPLASH_KEY = os.environ.get('UNSPLASH_KEY', '')
-DEBUG = os.environ.get('DEBUG', 'FALSE')
-debug = True if DEBUG == 'TRUE' else False
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+
+DEBUG = True if os.environ.get("DEBUG", "FALSE") == "TRUE" else False
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError(
-        'Please create .env.local file andinsert there UNSPLASH_KEY')
+    raise EnvironmentError("Please create .env.local file andinsert there UNSPLASH_KEY")
 
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['DEBUG'] = debug
+app.config["DEBUG"] = DEBUG
 
 
-@app.route('/new-image')
+@app.route("/new-image")
 def new_image():
-    word = request.args.get('query')
+    word = request.args.get("query")
 
-    headers = {
-        'Accept-Version': 'v1',
-        'Authorization': f'Client-ID {UNSPLASH_KEY}'
-    }
-    params = {'query': word}
+    headers = {"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"}
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
