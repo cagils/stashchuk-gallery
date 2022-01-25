9592709f8487305aeb84b59905c6354f9d36e53f
Cagil Seker <cagils@gmail.com>
2022-01-25 15:13:53 +0300

    Docker Compose volumes mapping for live coding

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant='primary' type='submit'>
-                  Search
+                  Search Image
                 </Button>
               </Col>
             </Row>
