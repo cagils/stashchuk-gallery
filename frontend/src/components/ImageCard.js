5b1852986dfe8ed3ce8e5c9e44c817de2ad0feb7
Cagil Seker <cagils@gmail.com>
2022-01-25 22:40:33 +0300

    Author name and link added

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Nav } from 'react-bootstrap';
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioUrl = image.user?.portfolio_url;
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant='top' src={image.urls.small} />
@@ -17,6 +19,15 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           </Button>
         )}
       </Card.Body>
+      <Card.Footer className='text-center text-muted'>
+        {authorPortfolioUrl ? (
+          <Nav.Link href={authorPortfolioUrl} target='_blank'>
+            {authorName}
+          </Nav.Link>
+        ) : (
+          authorName
+        )}
+      </Card.Footer>
     </Card>
   );
 };
