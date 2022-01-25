073df20219c24cca75d400a148a8bf4a3a7e40fd
Cagil Seker <cagils@gmail.com>
2022-01-25 18:36:07 +0300

    Mongo DB get post images api

--- api/main.py
+++ api/main.py
@@ -1,21 +1,22 @@
-import imp
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 
-
 DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file andinsert there UNSPLASH_KEY")
-
 
 app = Flask(__name__)
 CORS(app)
@@ -32,6 +33,22 @@ def new_image():
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from the database
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+
+    if request.method == "POST":
+        # save image in the database
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        result_id = result.inserted_id
+        return {"result": result_id}
 
 
 if __name__ == "__main__":
---
+++ api/mongo_client.py
@@ -0,0 +1,17 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+
+mongo_client = MongoClient(
+    host=MONGO_URL,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    port=MONGO_PORT,
+)
